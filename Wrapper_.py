5e934f938a858b131ad6d6c47115096b58e32d75
Vlad Luca <ioan-vlad.luca@golfgenius.com>
2023-03-05 14:19:10 +0200

    error state with digital ocean offers

--- Wrapper_.py
+++ Wrapper_.py
@@ -33,29 +33,29 @@ class Wrapper_:
         SMTsolver.init_problem(problem, "optimize", sb_option=symmetry_breaker)
         price, distr, runtime, a_mat, vms_type = SMTsolver.run()
 
-        if not runtime or runtime > 2400:
-            log("TESTING", "WARN", "Test aborted. Timeout")
-        else:
-            vm_specs = []
-            for index, (key, value) in enumerate(offers_json.items()):
-                if (index + 1) in vms_type:
-                    vm_spec = {key: value}
-                    vm_spec[key]["id"] = index + 1
-                    vm_specs.append(vm_spec)
+        # if not runtime or runtime > 2400:
+        #     log("TESTING", "WARN", "Test aborted. Timeout")
+        # else:
+        vm_specs = []
+        for index, (key, value) in enumerate(offers_json.items()):
+            if (index + 1) in vms_type:
+                vm_spec = {key: value}
+                vm_spec[key]["id"] = index + 1
+                vm_specs.append(vm_spec)
 
-            output = {
-                "output": {
-                    "min_price": sum(distr),
-                    "type_of_sym_br": symmetry_breaker or "None",
-                    "time (secs)": runtime,
-                    "types_of_VMs": [vm_type.as_long() for vm_type in vms_type],
-                    "prices_of_VMs": distr,
-                    "VMs specs": vm_specs,
-                    "assign_matr": [[el.as_long() for el in row] for row in a_mat],
-                }
+        output = {
+            "output": {
+                "min_price": sum(distr),
+                "type_of_sym_br": symmetry_breaker or "None",
+                "time (secs)": runtime,
+                "types_of_VMs": [vm_type.as_long() for vm_type in vms_type],
+                "prices_of_VMs": distr,
+                "VMs specs": vm_specs,
+                "assign_matr": [[el.as_long() for el in row] for row in a_mat],
             }
-            application_model_json.update(output)
-            return application_model_json
+        }
+        application_model_json.update(output)
+        return application_model_json
 
 
 wrapper = Wrapper_()
@@ -63,7 +63,7 @@ wrapper = Wrapper_()
 with open("Models/json/Wordpress.json", "r") as file:
     application = json.load(file)
 
-with open("Data/json/offers_20.json", "r") as file:
+with open("Data/json/digital_ocean_offers.json", "r") as file:
     offers_20 = json.load(file)
 
 result = wrapper.solve(application, offers_20, inst=3)
---
+++ scrapers/digital_ocean.py
@@ -0,0 +1,37 @@
+import json
+import subprocess
+
+OFFERS_DIR = "./Data/json"
+
+
+# "c64.0m976.0s1.0osLinuxp8.4030000000": {"cpu": 64, "memory": 976000, "storage": 1000, "operatingSystem": "Linux",
+# "price": 8403},
+
+def get_offers():
+    # [[{'name': 's-1vcpu-2gb', 'slug': 's-1vcpu-2gb'}]
+    result = subprocess.run("doctl kubernetes options sizes -o json".split(" "), stdout=subprocess.PIPE)
+    return json.loads(result.stdout.decode('utf-8'))
+
+
+def parse_offers(offers):
+    output = {}
+
+    i = 1
+    for offer in offers:
+        name = offer["name"]
+        output[name] = {
+            "cpu": int(name.replace("vcpu", "").split("-")[1]),
+            "memory": int(name.replace("gb", "").replace("GiB", "").split("-")[2]) * 1000,
+            "storage": 1000,
+            "operatingSystem": "Linux",
+            "price": 100 * i
+        }
+        i += 1
+
+    return output
+
+
+if __name__ == '__main__':
+    offers = get_offers()
+    with open(f"{OFFERS_DIR}/digital_ocean_offers.json", "w+") as f:
+        json.dump(parse_offers(offers), f)
